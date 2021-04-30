6e79618fbec758afcbcbf8b6dfa0e1673511c6e6
ManHunterITA <m4nu.91@gmail.com>
2021-04-30 16:09:18 +0000

    benchmark(FixedStack): added a little benchmark for FixedStack

---
+++ MHLab.Utilities.Benchmarks/Collections/Stackonly/FixedStackBenchmark.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using MHLab.Utilities.Collections.Stackonly;
+
+namespace MHLab.Utilities.Benchmarks.Collections.Stackonly
+{
+    [MemoryDiagnoser]
+    public class FixedStackBenchmark
+    {
+        public struct TestStruct
+        {
+            public int X;
+            public int Y;
+        }
+
+        [Benchmark]
+        public void StandardStack_Pushing_16_Elements()
+        {
+            var entry = new TestStruct() {X = 1, Y = 1};
+            var stack = new Stack<TestStruct>(16);
+
+            for (var i = 0; i < 16; i++)
+            {
+                stack.Push(entry);
+            }
+        }
+
+        [Benchmark]
+        public void FixedStack_Pushing_16_Elements()
+        {
+            var entry = new TestStruct() {X = 1, Y = 1};
+            
+            FixedStack<TestStruct> stack = stackalloc TestStruct[16];
+
+            for (var i = 0; i < 16; i++)
+            {
+                stack.Push(entry);
+            }
+        }
+    }
+}
---
+++ MHLab.Utilities.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using MHLab.Utilities.Benchmarks.Collections.Stackonly;
+
+namespace MHLab.Utilities.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<FixedStackBenchmark>();
+        }
+    }
+}
